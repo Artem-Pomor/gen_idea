210190d35548cfde5a50164af5e8e0f6f6509d0a
Artem Pomortsev <pomortezv@yandex.ru>
2025-04-22 17:42:47 +0300

    NI Fix: ci

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,41 +2,41 @@ import antfu from '@antfu/eslint-config';
 import nextPlugin from '@next/eslint-plugin-next';
 
 export default antfu(
-    {
-      react: true,
-      typescript: true,
+  {
+    react: true,
+    typescript: true,
 
-      lessOpinionated: true,
-      isInEditor: false,
+    lessOpinionated: true,
+    isInEditor: false,
 
-      stylistic: {
-        semi: true,
-      },
-
-      formatters: {
-        css: true,
-      },
-
-      ignores: ['migrations/**/*', 'next-env.d.ts'],
+    stylistic: {
+      semi: true,
     },
-    {
-      plugins: {
-        '@next/next': nextPlugin,
-      },
-      rules: {
-        ...nextPlugin.configs.recommended.rules,
-        ...nextPlugin.configs['core-web-vitals'].rules,
-      },
+
+    formatters: {
+      css: true,
     },
-    {
-      rules: {
-        'antfu/no-top-level-await': 'off', // Allow top-level await
-        'style/brace-style': ['error', '1tbs'], // Use the default brace style
-        'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
-        'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
-        'node/prefer-global/process': 'off', // Allow using `process.env`
-        'test/padding-around-all': 'error', // Add padding in test files
-        'test/prefer-lowercase-title': 'off', // Allow using uppercase titles in test titles
-      },
+
+    ignores: ['migrations/**/*', 'next-env.d.ts'],
+  },
+  {
+    plugins: {
+      '@next/next': nextPlugin,
     },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    rules: {
+      'antfu/no-top-level-await': 'off', // Allow top-level await
+      'style/brace-style': ['error', '1tbs'], // Use the default brace style
+      'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
+      'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+      'node/prefer-global/process': 'off', // Allow using `process.env`
+      'test/padding-around-all': 'error', // Add padding in test files
+      'test/prefer-lowercase-title': 'off', // Allow using uppercase titles in test titles
+    },
+  },
 );
