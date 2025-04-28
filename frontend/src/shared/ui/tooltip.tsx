30dd94ceff54e6e982f58414cd1088f6a4678f40
Artem-Pomor <pomortezv@yandex.ru>
2025-04-28 20:40:23 +0300

    Fixes #17: add home page

--- frontend/src/shared/ui/tooltip.tsx
+++ frontend/src/shared/ui/tooltip.tsx
@@ -6,12 +6,14 @@ import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import * as React from 'react';
 
 
+const TooltipComponent = TooltipPrimitive as unknown as React.ElementType;
+
 function TooltipProvider({
     delayDuration = 0,
     ...props
 }: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
     return (
-        <TooltipPrimitive
+        <TooltipComponent
             data-slot="tooltip-provider"
             delayDuration={delayDuration}
             {...props}
